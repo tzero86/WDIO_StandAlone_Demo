6cd83aa8bfce3720deb9a8ed99b39181f8a4e9b3
Elias <eliasmedina86@gmail.com>
2018-08-26 19:51:20 -0300

    Sample Repo with WDIO in standalone mode

---
+++ test.js
@@ -0,0 +1,14 @@
+let webdriverio = require('webdriverio');
+let options = { desiredCapabilities: { browserName: 'chrome'} };
+let client = webdriverio.remote(options);
+
+client
+    .init()
+    .url('https://youtube.com')
+    .setValue('#search-form #container', 'Morphine All Wrong')
+    .keys('Enter')
+    .pause(3000)
+    .waitForVisible("//div[@id='contents']//ytd-video-renderer[1]//div[1]//div[1]//div[1]//div[1]//h3[1]//a[1]")
+    .click("//div[@id='contents']//ytd-video-renderer[1]//div[1]//div[1]//div[1]//div[1]//h3[1]//a[1]")
+    .pause(90000)
+    .end();
